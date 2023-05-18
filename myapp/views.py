5ecd072fd4554279e48642a11d068221415d45f6
sorawit <thankzaki@hotmail.com>
2023-05-18 22:52:55 +0700

    show data on table

--- myapp/views.py
+++ myapp/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from myapp.models import Person
 
 # Create your views here.
 
 
 def index(request):
     # return HttpResponse('OMG')
-    name = 'Hanni'
-    age = 17
-    return render(request, 'index.html', {"name": name, "age": age})
+    all_person = Person.objects.all()
+    # all_person = Person.objects.filter(age=25)
+    return render(request, 'index.html', {"all_person": all_person})
 
 
 def about(request):
